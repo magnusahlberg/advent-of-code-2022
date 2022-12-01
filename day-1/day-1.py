9fe18978bd7f9826c3a902bc4a805458bc4a80af
Magnus Ahlberg <m@magnus.xyz>
2022-12-02 00:17:51 +0100

    Day 1 complete

---
+++ day-1/day-1.py
@@ -0,0 +1,27 @@
+import os
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
+file_name = os.path.join(file_dir, 'input/input.txt')
+file = open(file_name, 'r')
+
+lines = file.readlines()
+
+elf = 1
+calories = 0
+highest_calories = 0
+elf_with_highest_calories = 0
+
+
+for line in lines:
+    if line == "\n":
+        if calories > highest_calories:
+            elf_with_highest_calories = elf
+            highest_calories = calories
+            print(f'New Winner! Elf {elf_with_highest_calories} with {highest_calories}\n\n')
+        print(f'Elf {elf} with {calories}\n\n')
+        elf += 1
+        calories = 0
+    else:
+        calories += int(line)
+
+print(f'Elf: {elf_with_highest_calories}\nCalories: {highest_calories}\n')
