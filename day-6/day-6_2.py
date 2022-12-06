6589fedaccf3138716d82a2ae32fe2eef5eb6935
Magnus Ahlberg <m@magnus.xyz>
2022-12-06 10:26:43 +0100

    Day 6

---
+++ day-6/day-6_2.py
@@ -0,0 +1,22 @@
+import os
+import string
+from functools import reduce 
+
+def read_input(rel_path: str):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    file_name = os.path.join(file_dir, rel_path)
+    file = open(file_name, 'r')
+    return file.read()
+
+distinct_chars = 14
+
+input = read_input('input/input.txt')
+
+for i in range(distinct_chars,len(input)):
+    seek_string = input[i-distinct_chars:i]
+
+    count = sum([seek_string.count(char) for char in seek_string])
+
+    if count == distinct_chars:
+        print(i)
+        break
